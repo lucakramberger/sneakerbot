f34b5192fcebc3945111b0b628b47d0f2808f240
Lucaaa <lpmitfreecraft@gmail.com>
2020-12-28 12:46:12 +0100

    generating link and login is working

--- NikeSnkrsBot/src/Task.java
+++ NikeSnkrsBot/src/Task.java
@@ -1,7 +1,15 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Task extends Thread{
     String link;
@@ -9,14 +17,22 @@ public class Task extends Thread{
     String size;
     WebDriver driver;
     ChromeOptions chromeOptions;
+    String productId;
+    Profile profile;
 
     Task(){
 
     }
+    //<meta data-react-helmet="true" name="branch:deeplink:productId" content="2eaeaf7d-b6ef-5285-b2d5-c2e799f86e0f">
+    //https://www.nike.com/at/launch/t/air-jordan-11-adapt-eu?size=11&productId=2eaeaf7d-b6ef-5285-b2d5-c2e799f86e0f
+    //https://www.nike.com/at/launch/t/air-jordan-11-adapt-eu?size=11&productId=3f9fce22-e8fd-5e79-94ca-2a0f5e18e141
 
-    Task(String link, String ip, String port, String size){
+    //<meta data-react-helmet="true" name="branch:deeplink:productId" content="2eaeaf7d-b6ef-5285-b2d5-c2e799f86e0f">
+
+    Task(String link, String ip, String port, String size, Profile profile){
         this.link = link;
         this.proxy = new Proxy();
+        this.profile = profile;
         this.proxy.setHttpProxy("<"+ip+":"+port+">");
 
         this.size = size;
@@ -30,10 +46,53 @@ public class Task extends Thread{
     public void run() {
         try {
             this.driver = new ChromeDriver(this.chromeOptions);
-            this.driver.get(this.link);
+            this.productId = getProductId(this.link);
+
+            String checkoutLink = generateLink(this.link, this.size, this.productId);
+
+            this.driver.get(checkoutLink);
+            sleep(1000);
+
+            if (this.driver.findElement(By.name("emailAddress")) != null){
+                login(this.driver);
+                this.driver.get(checkoutLink);
+            }
 
         }catch (Exception e){
             System.out.println(e);
         }
+    }
+
+    private String generateLink(String link, String size, String productId){
+        return link+"?size="+size+"&productId="+productId;
+    }
+
+    private String getProductId(String websiteLink) throws IOException {
+        URL oracle = new URL(websiteLink);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+
+        String totalInput = "";
+        String inputLine = "";
+        while ((inputLine = in.readLine()) != null)
+            totalInput += inputLine;
+        in.close();
+
+        String[] splitted = totalInput.split("<meta data-react-helmet=\"true\" name=\"branch:deeplink:productId\" content=\"");
+
+        String productId = splitted[1].split("\"/>")[0];
+        return productId;
+    }
+
+    private void login(WebDriver webdriver){
+        WebElement email = webdriver.findElement(By.name("emailAddress"));
+        email.sendKeys(this.profile.username);
+
+        WebElement password = webdriver.findElement(By.name("password"));
+        password.sendKeys(this.profile.password);
+
+
+        WebElement submit = webdriver.findElement(By.cssSelector("div[class='nike-unite-submit-button loginSubmit nike-unite-component']"));
+        submit.click();
     }
 }
--- NikeSnkrsBot/src/main.java
+++ NikeSnkrsBot/src/main.java
@@ -2,7 +2,10 @@ public class main {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
-        Task task = new Task("https://www.nike.com/at/launch/t/kobe-6-protro-green-apple", "178.115.231.163","8080", "45");
+
+        Profile profile = new Profile("spl3shy@gmail.com","!Freddy1234!");
+
+        Task task = new Task("https://www.nike.com/at/launch/t/air-jordan-11-adapt-eu", "178.115.231.163","8080", "11", profile);
         task.start();
     }
 }
